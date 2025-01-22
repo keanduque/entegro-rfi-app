88c83b9ef72fdf7bf4c098387020d15bfa1800c7
Kean Duque <kduque@entegro.ie>
2025-01-22 16:02:14 +0000

    update

--- server/db.js
+++ server/db.js
@@ -9,6 +9,8 @@ const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DB,
+  max: 5, // Limit connections to avoid overloading
+  idleTimeoutMillis: 10000, // Close idle clients after 10 seconds
 });
 
 module.exports = pool;
